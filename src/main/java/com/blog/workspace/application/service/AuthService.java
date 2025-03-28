166e2275c7c0b75c3b9836539283fae6694a9c13
eedo_y <doup2001@naver.com>
2025-03-29 00:03:28 +0900

    ✨ feat : 소셜 로그인 기능 구현

---
+++ src/main/java/com/blog/workspace/application/service/AuthService.java
@@ -0,0 +1,99 @@
+package com.blog.workspace.application.service;
+
+import com.blog.workspace.adapter.in.web.dto.request.AuthFirstTimeRequest;
+import com.blog.workspace.adapter.out.oauth.OAuthUserInfo;
+import com.blog.workspace.adapter.out.oauth.kakao.KaKaoApiClient;
+import com.blog.workspace.application.in.auth.AuthUserUseCase;
+import com.blog.workspace.application.out.auth.OAuthLoginParams;
+import com.blog.workspace.application.out.user.UserPort;
+import com.blog.workspace.domain.user.User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class AuthService implements AuthUserUseCase {
+
+    private final KaKaoApiClient clients;
+    private final UserPort userPort;
+
+    public AuthService(KaKaoApiClient clients, UserPort userPort) {
+        this.clients = clients;
+        this.userPort = userPort;
+    }
+
+    @Override
+    public String login(OAuthLoginParams params) {
+
+        // 토큰 정보 받아오기
+        String accessToken = clients.requestAccessToken(params);
+
+        // 해당 토큰을 바탕으로 정보 요청
+        OAuthUserInfo oAuthUserInfo = clients.requestOAuthInfo(accessToken);
+
+        // 최초 로그인이 아니라면, 정보 반환
+        if (!checkFirstLogin(oAuthUserInfo)){
+            return loadUser(oAuthUserInfo).getId().toString(); // 기존 로그인 처리
+        } else {
+            // 저장하는 로직은 수행한다.
+            User user = newUser(oAuthUserInfo);
+            return "최초 로그인 추가정보 기입 필요: " + user.getEmail();
+        }
+
+    }
+
+    @Override
+    public User updateInfo(String email, AuthFirstTimeRequest request) {
+
+        User user = userPort.findUserByEmail(email)
+                .orElseThrow(() -> new NoSuchElementException("해당 유저는 존재하지 않습니다."));
+
+        // 닉네임이 요청에 있으면 수정
+        if (request.getNickname() != null && !request.getNickname().isEmpty()) {
+            user.changeNickname(request.getNickname());
+        }
+
+        // 생일이 요청에 있으면 수정
+        if (request.getBirthday() != null && !request.getBirthday().isEmpty()) {
+            user.changeBirthday(request.getBirthday());
+        }
+
+        // 설명이 요청에 있으면 수정
+        if (request.getDescription() != null && !request.getDescription().isEmpty()) {
+            user.changeDescription(request.getDescription());
+        }
+
+        // 이미지 URL이 요청에 있으면 수정
+        if (request.getImageUrl() != null && !request.getImageUrl().isEmpty()) {
+            user.changeImageUrl(request.getImageUrl());
+        }
+
+
+        return userPort.updateUser(user);
+    }
+
+    // 최초 로그인 여부 체크
+    private boolean checkFirstLogin(OAuthUserInfo oAuthUserInfo) {
+        Optional<User> user = userPort.findUserByEmail(oAuthUserInfo.getEmail());
+        return !user.isPresent();
+    }
+
+    private User loadUser(OAuthUserInfo oAuthUserInfo) {
+        return userPort.findUserByEmail(oAuthUserInfo.getEmail())
+                .orElse(null);
+    }
+
+    private User newUser(OAuthUserInfo oAuthUserInfo) {
+        // 생성시간 수정
+        LocalDateTime now = LocalDateTime.now();
+
+        // 이메일, 이름, 이미지 정보만 포함되어있는 유저 정보 저장하기
+        User user = new User(oAuthUserInfo.getEmail(), oAuthUserInfo.getUserName(), oAuthUserInfo.getImageUrl(), now, now);
+
+        return userPort.saveUser(user);
+    }
+}
