b2cce786fa31668dbfe9915469e3c7c295ff3b32
eedo_y <doup2001@naver.com>
2025-03-29 00:28:35 +0900

    ♻️ refactor : 예외 클래스 생성자 수정

--- src/main/java/com/blog/workspace/application/service/exception/DuplicationUserException.java
+++ src/main/java/com/blog/workspace/application/service/exception/DuplicationUserException.java
@@ -2,9 +2,7 @@ package com.blog.workspace.application.service.exception;
 
 public class DuplicationUserException extends RuntimeException {
 
-    private final String message;
-
     public DuplicationUserException(String message) {
-        this.message = message;
+        super(message);
     }
 }
--- src/main/java/com/blog/workspace/application/service/exception/NotSamePasswordException.java
+++ src/main/java/com/blog/workspace/application/service/exception/NotSamePasswordException.java
@@ -1,9 +1,8 @@
 package com.blog.workspace.application.service.exception;
 
 public class NotSamePasswordException extends RuntimeException {
-    private final String message;
 
     public NotSamePasswordException(String message) {
-        this.message = message;
+        super(message);
     }
 }
