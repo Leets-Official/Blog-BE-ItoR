c9ed18af48136c34e155470631c03d3010c9a965
seokjun01 <suckjungod@naver.com>
2025-03-29 16:54:33 +0900

    [feat] 유저 도메인 필드 구성

--- src/main/java/com/blog/domain/user/domain/User.java
+++ src/main/java/com/blog/domain/user/domain/User.java
@@ -1,4 +1,98 @@
 package com.blog.domain.user.domain;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class User {
+
+	private int userId;
+	private String name;
+	private String nickName;
+	private String email;
+	private String password;
+	private UserType userType;
+	private LocalDate birthDate;
+	private String introduce;
+	private String profileImageUrl;
+	private LocalDateTime createdAt;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public UserType getUserType() {
+		return userType;
+	}
+
+	public void setUserType(UserType userType) {
+		this.userType = userType;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getIntroduce() {
+		return introduce;
+	}
+
+	public void setIntroduce(String introduce) {
+		this.introduce = introduce;
+	}
+
+	public String getProfileImageUrl() {
+		return profileImageUrl;
+	}
+
+	public void setProfileImageUrl(String profileImageUrl) {
+		this.profileImageUrl = profileImageUrl;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
 }
