b995a20909a1bd16734af4b3c106c0bbe9895053
seokjun01 <suckjungod@naver.com>
2025-03-30 16:12:48 +0900

    [feat] 임시 Dto 파일 생성

--- src/main/java/com/blog/global/auth/dto/LoginResponseDto.java
+++ src/main/java/com/blog/global/auth/dto/LoginResponseDto.java
@@ -1,4 +1,20 @@
 package com.blog.global.auth.dto;
 
 public class LoginResponseDto {
+
+	private final String email;
+	private String password;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public LoginResponseDto(String email, String password) {
+		this.email = email;
+		this.password = password;
+	}
 }
