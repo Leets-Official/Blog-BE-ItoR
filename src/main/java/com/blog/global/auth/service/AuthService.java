2364b8af3d9c7660c2f8541ad663049922c0205d
seokjun01 <suckjungod@naver.com>
2025-03-31 15:51:06 +0900

    [add] 회원가입 서비스 로직 추가

--- src/main/java/com/blog/global/auth/service/AuthService.java
+++ src/main/java/com/blog/global/auth/service/AuthService.java
@@ -1,9 +1,13 @@
 package com.blog.global.auth.service;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Service;
 
 import com.blog.domain.user.domain.User;
 import com.blog.domain.user.repository.UserRepository;
+import com.blog.global.auth.dto.SignUpRequestDto;
+import com.blog.global.auth.dto.SignUpResponseDto;
 import com.blog.global.auth.jwt.JwtUtil;
 import com.blog.global.config.error.ErrorCode;
 import com.blog.global.config.error.exception.CommonException;
@@ -31,5 +35,32 @@ public class AuthService {
 
 		// 토큰 생성 후 반환
 		return jwtUtil.createToken(String.valueOf(user.getUserId()));
+	}
+
+	public SignUpResponseDto signUp(SignUpRequestDto signUpRequestDto) {
+
+		if (userRepository.existsByEmail(signUpRequestDto.getEmail())) {
+			throw new CommonException(ErrorCode.EMAIL_ALREADY_EXISTS);
+		}
+
+		if (!signUpRequestDto.getPassword().equals(signUpRequestDto.getPasswordConfirm())) {
+			throw new CommonException(ErrorCode.PASSWORD_MISMATCH);
+		}
+
+		User user = User.createEmailUser(
+			signUpRequestDto.getName(),
+			signUpRequestDto.getNickName(),
+			signUpRequestDto.getEmail(),
+			signUpRequestDto.getPassword(),
+			signUpRequestDto.getBirthDate(),
+			signUpRequestDto.getIntroduce(),
+			signUpRequestDto.getProfileImageUrl()
+		);
+
+		userRepository.save(user);
+
+		return new SignUpResponseDto("회원가입 성공!");
+	}
+
 	}
 }
