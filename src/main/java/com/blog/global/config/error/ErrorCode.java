45037f9528418c326a37ca6597d5584a6dc179a6
seokjun01 <suckjungod@naver.com>
2025-03-31 15:50:39 +0900

    [add] 에러코드 추가

--- src/main/java/com/blog/global/config/error/ErrorCode.java
+++ src/main/java/com/blog/global/config/error/ErrorCode.java
@@ -3,7 +3,9 @@ package com.blog.global.config.error;
 public enum ErrorCode {
 
 	USER_NOT_FOUND(400, "USER_NOT_FOUND", "사용자를 찾을 수 없습니다."),
-	INVALID_PASSWORD(401, "INVALID_PASSWORD", "비밀번호가 일치하지 않습니다.");
+	INVALID_PASSWORD(401, "INVALID_PASSWORD", "비밀번호가 일치하지 않습니다."),
+	PASSWORD_MISMATCH(400, "PASSWORD_MISMATCH", "비밀번호가 일치하지 않습니다."),
+	EMAIL_ALREADY_EXISTS(400, "EMAIL_ALREADY_EXISTS", "이미 사용 중인 이메일입니다.");
 
 	private final int status;
 	private final String code;
