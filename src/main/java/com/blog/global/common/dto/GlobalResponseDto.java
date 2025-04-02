a5b19ce7448d4c9349104eb96b25cd74a0ab3272
seokjun01 <suckjungod@naver.com>
2025-04-02 19:02:56 +0900

    [add] 공통응답 필드 추가

--- src/main/java/com/blog/global/common/dto/GlobalResponseDto.java
+++ src/main/java/com/blog/global/common/dto/GlobalResponseDto.java
@@ -8,41 +8,52 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 public class GlobalResponseDto<T> {
 
 	private boolean isSuccess;
-	private int code;
+	private int status;
 	private LocalDateTime timestamp;
 	private String message;
+	private String errorCode;
 
 	@JsonInclude(JsonInclude.Include.NON_NULL)
 	private T result;
 
 	public GlobalResponseDto() {}
 
-	public GlobalResponseDto(boolean isSuccess, int code, String message, T result) {
+	public GlobalResponseDto(boolean isSuccess, int status, String message, String errorCode, T result) {
 		this.isSuccess = isSuccess;
-		this.code = code;
+		this.status = status;
 		this.timestamp = LocalDateTime.now();
 		this.message = message;
+		this.errorCode = errorCode;
 		this.result = result;
 	}
 
+
 	public static <T> GlobalResponseDto<T> success(T result) {
-		return new GlobalResponseDto<>(true, 200, "성공", result);
+		return new GlobalResponseDto<>(true, 200, "성공", null, result);
 	}
 
 	public static <T> GlobalResponseDto<T> success() {
-		return new GlobalResponseDto<>(true, 200, "성공", null);
+		return new GlobalResponseDto<>(true, 200, "성공", null, null);
 	}
 
 	public static <T> GlobalResponseDto<T> fail(ErrorCode errorCode) {
-		return new GlobalResponseDto<>(false, errorCode.getStatus(), errorCode.getMessage(), null);
+		return new GlobalResponseDto<>(false,
+			errorCode.getStatus(),
+			errorCode.getMessage(),
+			errorCode.getCode(),
+			null);
 	}
 
 	public boolean isSuccess() {
 		return isSuccess;
 	}
 
-	public int getCode() {
-		return code;
+	public int getStatus() {
+		return status;
+	}
+
+	public String getErrorCode() {
+		return errorCode;
 	}
 
 	public LocalDateTime getTimestamp() {
