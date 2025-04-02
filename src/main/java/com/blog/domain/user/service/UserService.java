dceb34d27eb22fa47cb1af49a96485182dfa83ae
seokjun01 <suckjungod@naver.com>
2025-04-02 18:02:59 +0900

    [del] 테스트 코드 주석 제거

--- src/main/java/com/blog/domain/user/service/UserService.java
+++ src/main/java/com/blog/domain/user/service/UserService.java
@@ -18,22 +18,4 @@ public class UserService {
 		this.userRepository = userRepository;
 	}
 
-/*  DB테스트 유저 생성 테스트 코드
-	public void createTestUser() {
-		User user = new User();
-		user.setName("홍길동");
-		user.setNickName("길동이");
-		user.setEmail("test@example.com");
-		user.setPassword("1234"); // 실제론 암호화 필요
-		user.setUserType(UserType.LOCAL);
-		user.setBirthDate(LocalDate.of(2001, 10, 15));
-		user.setIntroduce("안녕하세요, 테스트 유저입니다!");
-		user.setProfileImageUrl("https://example.com/profile.jpg");
-		user.setCreatedAt(LocalDateTime.now());
-
-		userRepository.save(user);
-		System.out.println("✅ 테스트 유저 저장 완료!");
-	}
-
- */
 }
