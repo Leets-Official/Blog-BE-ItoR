7d9b2e4cacf627cd20d5b4daaa9d2a74ee4e57d7
seokjun01 <suckjungod@naver.com>
2025-04-04 02:23:12 +0900

    [feat] Post&PostImage 도메인 구현

--- src/main/java/com/blog/domain/post/domain/PostImage.java
+++ src/main/java/com/blog/domain/post/domain/PostImage.java
@@ -1,4 +1,31 @@
 package com.blog.domain.post.domain;
 
+import java.time.LocalDateTime;
+
 public class PostImage {
+	private int imageId;
+	private int postId;
+	private String imageUrl;
+	private LocalDateTime createdAt;
+
+	public PostImage() {}
+
+	public PostImage(int imageId, int postId, String imageUrl) {
+		this.imageId = imageId;
+		this.postId = postId;
+		this.imageUrl = imageUrl;
+	}
+
+	public int getPostId() {
+		return postId;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public int getImageId() {
+		return imageId;
+	}
+
 }
