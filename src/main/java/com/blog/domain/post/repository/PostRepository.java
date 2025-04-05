9f185dc1b39a29311903a857183f33942a277319
seokjun01 <suckjungod@naver.com>
2025-04-05 14:37:12 +0900

    [feat] Post리포지터리 인터페이스 구현

--- src/main/java/com/blog/domain/post/repository/PostRepository.java
+++ src/main/java/com/blog/domain/post/repository/PostRepository.java
@@ -1,4 +1,17 @@
 package com.blog.domain.post.repository;
 
-public class PostRepository {
+import java.sql.Connection;
+import java.util.List;
+
+import com.blog.domain.post.domain.Post;
+
+public interface PostRepository {
+
+	boolean insert(Connection conn, Post post);
+	Post findById(Connection conn, int postId);
+	List<Post> findPage(Connection conn, int offset, int size);
+	int countAll (Connection conn);
+	boolean update(Connection conn, Post post);
+	boolean Softdelete(Connection conn, int postId);
+
 }
