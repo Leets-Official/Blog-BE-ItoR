87cf13e4eac140434fa57e63ce2daaa16df3f191
seokjun01 <suckjungod@naver.com>
2025-04-06 01:01:54 +0900

    [add] 에러코드 추가

--- src/main/java/com/blog/global/config/error/ErrorCode.java
+++ src/main/java/com/blog/global/config/error/ErrorCode.java
@@ -16,7 +16,17 @@ public enum ErrorCode {
 	KAKAO_TOKEN_REQUEST_FAILED(500, "TOKEN_REQUEST_FAILED", "카카오 토큰 요청에 실패했습니다."),
 	KAKAO_TOKEN_PARSE_FAILED(500, "TOKEN_PARSE_FAILED", "카카오 토큰 응답 파싱에 실패했습니다."),
 	KAKAO_USERINFO_REQUEST_FAILED(500, "USERINFO_REQUEST_FAILED", "카카오 사용자 정보 요청에 실패했습니다."),
-	KAKAO_USERINFO_PARSE_FAILED(500, "USERINFO_PARSE_FAILED", "카카오 사용자 정보 파싱에 실패했습니다.");
+	KAKAO_USERINFO_PARSE_FAILED(500, "USERINFO_PARSE_FAILED", "카카오 사용자 정보 파싱에 실패했습니다."),
+
+	//post관련
+	POST_NOT_FOUND(404, "POST_NOT_FOUND", "게시글을 찾을 수 없습니다"),
+	POST_UPDATE_FAILED(500,"POST_UPDATE_FAILED","게시글 수정에 실패했습니다"),
+	POST_DELETE_FAILED(500,"POST_DELETED_FAILED", "게시글 삭제에 실패했습니다"),
+	POST_ALREADY_DELETED(400, "POST_ALREADY_DELETED", "이미 삭제된 게시글입니다."),
+	FORBIDDEN_POST_ACCESS(403, "FORBIDDEN_POST_ACCESS", "해당 게시글에 대한 권한이 없습니다."),
+
+	//파싱관련
+	SERIALIZE_FAILED(500, "SERIALIZE_FAILED", "직렬화에 오류가 발생했습니다.");
 
 	private final int status;
 	private final String code;
