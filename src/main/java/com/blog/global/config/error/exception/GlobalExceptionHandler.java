19463409a063a445250edaa46e33de891e73397a
seokjun01 <suckjungod@naver.com>
2025-04-07 15:48:47 +0900

    [add] 메소드 유효성 공통예외 추가

--- src/main/java/com/blog/global/config/error/exception/GlobalExceptionHandler.java
+++ src/main/java/com/blog/global/config/error/exception/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.blog.global.config.error.exception;
 
+import java.util.stream.Collectors;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -21,6 +24,15 @@ public class GlobalExceptionHandler {
 		return ResponseEntity
 			.status(500)
 			.body(new GlobalResponseDto<>(false, 500, "서버 내부 오류가 발생했습니다.", "INTERNAL_SERVER_ERROR", null));
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<GlobalResponseDto<Void>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+		// 필드 에러 메시지를 하나의 문자열로 결합
+		String errorMessage = ex.getBindingResult().getFieldErrors().stream()
+			.map(error -> error.getDefaultMessage())
+			.collect(Collectors.joining(", "));
+		return ResponseEntity.badRequest().body(GlobalResponseDto.error(errorMessage));
 	}
 
 }
