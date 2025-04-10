e4042ec0af49531ee4077535ad2435e2409cd883
eedo_y <doup2001@naver.com>
2025-04-11 01:37:49 +0900

    ✨ feat : 댓글 관련 인터페이스 정의

--- src/main/java/com/blog/workspace/application/in/comment/CommentUseCase.java
+++ src/main/java/com/blog/workspace/application/in/comment/CommentUseCase.java
@@ -1,5 +1,11 @@
 package com.blog.workspace.application.in.comment;
 
+import com.blog.common.response.page.Page;
+import com.blog.common.response.page.Pageable;
+import com.blog.workspace.adapter.in.web.dto.request.CommentRequest;
+import com.blog.workspace.adapter.in.web.dto.request.CommentUpdateRequest;
+import com.blog.workspace.domain.comment.Comment;
+
 public interface CommentUseCase {
 
     /*
@@ -7,5 +13,18 @@ public interface CommentUseCase {
         사용자는 자신의 댓글만 수정, 삭제할 수 있어야 합니다.
         댓글에는 댓글을 달수 없습니다(단 원하는 경우 구현해도 괜찮습니다)
      */
+
+    /// 댓글 작성
+    Comment saveComment(CommentRequest request, Long userId);
+
+    /// 댓글 조회
+    // 게시판에 따른 댓글 목록 조회 (페이징 처리10개)
+    Page<Comment> loadCommentsByPost(Pageable pageable, Long postId);
+
+    /// 댓글 수정
+    Comment updateComment(Long commentId, Long userId, CommentUpdateRequest request);
+
+    /// 댓글 삭제
+    void deleteComment(Long userId, Long commentId);
 
 }
