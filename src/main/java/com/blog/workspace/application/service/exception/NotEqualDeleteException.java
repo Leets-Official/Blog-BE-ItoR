3c015f0643e5b66d4de987a9de5b3a5c7b9da491
eedo_y <doup2001@naver.com>
2025-04-12 00:14:35 +0900

    ♻️ refactor : 글로벌 예외 핸들러 간소화 - 3주차 코드 리뷰 반영

---
+++ src/main/java/com/blog/workspace/application/service/exception/NotEqualDeleteException.java
@@ -0,0 +1,7 @@
+package com.blog.workspace.application.service.exception;
+
+public class NotEqualDeleteException extends RuntimeException {
+    public NotEqualDeleteException(String message) {
+        super(message);
+    }
+}
