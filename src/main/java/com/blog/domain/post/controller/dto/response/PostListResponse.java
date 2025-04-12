ac9f3a9ed658fa0b074645a70cff1bc4e5681cf8
hwangjiyoung02 <hjy020714@gmail.com>
2025-04-12 14:03:26 +0900

    ⚒️refactoring : 중복 로직 PostContentDto에 static 메서드 구현

--- src/main/java/com/blog/domain/post/controller/dto/response/PostListResponse.java
+++ src/main/java/com/blog/domain/post/controller/dto/response/PostListResponse.java
@@ -5,23 +5,22 @@ import com.blog.domain.post.domain.ContentType;
 import com.blog.domain.post.domain.Post;
 import com.blog.domain.post.domain.PostContent;
 
-import java.time.LocalDate;
-import java.util.Comparator;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public record PostListResponse(
-        Long postId,
+        long postId,
         String title,
         List<PostContentDto> postContents,
         int commentCount,
         String imageUrl,
-        LocalDate createdAt
+        LocalDateTime createdAt
 ) {
 
     public static PostListResponse from(Post post, List<PostContent> postContents) {
-        List<PostContentDto> postContentDtos = toPostContentDtos(postContents);
-        String thumbnailUrl = extractThumbnailUrl(postContentDtos);
+        List<PostContentDto> postContentDtos = PostContentDto.from(postContents);
+        String thumbnailUrl = PostContentDto.extractThumbnailUrl(postContentDtos);
         return new PostListResponse(
                 post.getId(),
                 post.getTitle(),
@@ -32,22 +31,4 @@ public record PostListResponse(
         );
     }
 
-    // postContents에서 postContentDto로 변환
-    private static List<PostContentDto> toPostContentDtos(List<PostContent> postContents) {
-        return postContents.stream()
-                .map(content -> new PostContentDto(
-                        ContentType.valueOf(content.getType().name()),
-                        content.getContent()
-                ))
-                .collect(Collectors.toList());
-    }
-
-    // content에서 추출된 첫 이미지를 썸네일로 설정
-    private static String extractThumbnailUrl(List<PostContentDto> postContents) {
-        return postContents.stream()
-                .filter(dto -> dto.type() == ContentType.IMAGE)
-                .map(PostContentDto::data)
-                .findFirst()
-                .orElse(null);
-    }
 }
--- src/main/java/com/blog/domain/post/controller/dto/response/PostResponse.java
+++ src/main/java/com/blog/domain/post/controller/dto/response/PostResponse.java
@@ -6,29 +6,27 @@ import com.blog.domain.post.domain.Post;
 import com.blog.domain.post.domain.PostContent;
 import com.blog.domain.user.domain.User;
 
-import java.time.LocalDate;
-import java.util.Comparator;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static com.blog.domain.post.controller.dto.response.AuthorSummary.toAuthorSummary;
 
 public record PostResponse(
-        Long postId,
+        long postId,
         String title,
         List<PostContentDto> postContents,
         int commentCount,
         boolean isOwner,
 
         String imageUrl,
-        LocalDate createdAt,
+        LocalDateTime createdAt,
         AuthorSummary authorSummary
 
 ) {
-
     public static PostResponse from(Post post, List<PostContent> postContent, User user, boolean isOwner) {
-        List<PostContentDto> postContents = toPostContentDtos(postContent);
-        String thumbnailUrl = extractThumbnailUrl(postContents);
+        List<PostContentDto> postContents = PostContentDto.from(postContent);
+        String thumbnailUrl = PostContentDto.extractThumbnailUrl(postContents);
         AuthorSummary authorSummary = toAuthorSummary(user);
 
         return new PostResponse(
@@ -43,21 +41,4 @@ public record PostResponse(
         );
     }
 
-    // postContents에서 postContentDto로 변환
-    private static List<PostContentDto> toPostContentDtos(List<PostContent> postContent) {
-        return postContent.stream()
-                .map(content -> new PostContentDto(
-                        ContentType.valueOf(content.getType().name()),
-                        content.getContent()
-                ))
-                .collect(Collectors.toList());
-    }
-
-    private static String extractThumbnailUrl(List<PostContentDto> postContents) {
-        return postContents.stream()
-                .filter(dto -> dto.type() == ContentType.IMAGE)
-                .map(PostContentDto::data)
-                .findFirst()
-                .orElse(null);
-    }
 }
