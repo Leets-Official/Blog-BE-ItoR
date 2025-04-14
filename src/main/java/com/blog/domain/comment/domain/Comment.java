06094a7e11242af4bae5933f7ea1e8a82169c252
seokjun01 <suckjungod@naver.com>
2025-04-14 14:56:42 +0900

    [feat] 댓글 엔티티 구현

--- src/main/java/com/blog/domain/comment/domain/Comment.java
+++ src/main/java/com/blog/domain/comment/domain/Comment.java
@@ -1,4 +1,43 @@
 package com.blog.domain.comment.domain;
 
+import java.time.LocalDateTime;
+
 public class Comment {
+
+	private int commentId;
+	private int userId;
+	private int postId;
+	private String content;
+	private LocalDateTime createdAt;
+	private LocalDateTime modifiedAt;
+	private LocalDateTime deletedAt;
+
+	public int getCommentId() {
+		return commentId;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public int getPostId() {
+		return postId;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public LocalDateTime getModifiedAt() {
+		return modifiedAt;
+	}
+
+	public LocalDateTime getDeletedAt() {
+		return deletedAt;
+	}
+
 }
