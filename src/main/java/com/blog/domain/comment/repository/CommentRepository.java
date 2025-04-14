a357b4831b3f12ca33ffb27c9904ff3f4d8bb43d
seokjun01 <suckjungod@naver.com>
2025-04-14 15:35:14 +0900

    [feat] 댓글 관련 리포지토리 구현

--- src/main/java/com/blog/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/blog/domain/comment/repository/CommentRepository.java
@@ -1,4 +1,16 @@
 package com.blog.domain.comment.repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.blog.domain.comment.domain.Comment;
+
 public interface CommentRepository {
+	//어떤 메서드들이 필요할까 CRUD - 고민하기
+	void save(Comment comment);
+	List<Comment> findByPostId(int postId);
+	boolean update(Comment comment);
+	boolean delete(int commentId);
+
+
 }
--- src/main/java/com/blog/domain/comment/repository/CommentRepositoryImpl.java
+++ src/main/java/com/blog/domain/comment/repository/CommentRepositoryImpl.java
@@ -1,4 +1,67 @@
 package com.blog.domain.comment.repository;
 
-public class CommentRepositoryImpl {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.blog.domain.comment.domain.Comment;
+import com.blog.domain.post.domain.Post;
+
+@Repository
+public class CommentRepositoryImpl implements CommentRepository {
+
+	private	final JdbcTemplate jdbcTemplate;
+
+	public CommentRepositoryImpl(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	@Override
+	public void save(Comment comment) {
+		String sql = "INSERT INTO comment (userId, postId, content, createdAt, modifiedAt ) VALUES (?,?,?,?,?))";
+		jdbcTemplate.update(sql,
+			comment.getUserId(),
+			comment.getPostId(),
+			comment.getContent(),
+			comment.getCreatedAt(),
+			comment.getModifiedAt());
+
+	}
+
+	@Override
+	public List<Comment> findByPostId(int postId) {
+		String sql = "SELECT * FROM comment WHERE postId = ?";
+		return jdbcTemplate.query(sql, (rs, rowNum) -> mapRow(rs), postId);
+
+	}
+
+	@Override
+	public boolean update(Comment comment) {
+		String sql = "UPDATE comment SET content = ? WHERE commentId =?";
+		return jdbcTemplate.update(sql, comment.getContent(), comment.getCommentId()) > 0;
+	}
+
+	@Override
+	public boolean delete(int commentId) {
+		String sql = "DELETE FROM comment WHERE commentId = ?";
+		return jdbcTemplate.update(sql, commentId) > 0;
+
+	}
+
+
+	private Comment mapRow(ResultSet rs) throws SQLException {
+		return new Comment(
+			rs.getInt("commentId"),
+			rs.getInt("userId"),
+			rs.getInt("postId"),
+			rs.getString("content"),
+			rs.getTimestamp("createdAt").toLocalDateTime(),
+			rs.getTimestamp("modifiedAt").toLocalDateTime(),
+			rs.getTimestamp("deletedAt") != null ? rs.getTimestamp("deletedAt").toLocalDateTime() : null
+		);
+	}
+
 }
