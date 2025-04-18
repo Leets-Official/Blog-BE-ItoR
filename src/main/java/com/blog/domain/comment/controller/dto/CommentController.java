1d2e665cec3f23a28fd7aae0d3522dc5767d5327
hwangjiyoung02 <hjy020714@gmail.com>
2025-04-18 15:21:02 +0900

    ✨ feat : Comment controller생성

---
+++ src/main/java/com/blog/domain/comment/controller/dto/CommentController.java
@@ -0,0 +1,46 @@
+package com.blog.domain.comment.controller.dto;
+
+
+import com.blog.domain.comment.controller.dto.request.CommentRequest;
+import com.blog.domain.comment.controller.dto.request.CommentUpdatedRequest;
+import com.blog.domain.comment.service.CommentService;
+import com.blog.global.response.ApiResponse;
+import com.blog.global.security.aop.GetUserId;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/comment")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    // 댓글 등록
+    @PostMapping
+    public ApiResponse<String> registerComment(@GetUserId Long userId, @Valid @RequestBody CommentRequest request) {
+        commentService.registerComment(request, userId);
+        return ApiResponse.ok("정상적으로 등록되었습니다.");
+    }
+
+    //댓글 수정
+    @PutMapping("/{commentId}")
+    public ApiResponse<String> updateComment(@GetUserId Long userId, @PathVariable Long commentId, @Valid @RequestBody CommentUpdatedRequest request) {
+        commentService.updateComment(userId, commentId, request);
+        return ApiResponse.ok("정상적으로 수정되었습니다.");
+
+    }
+
+    //댓글 삭제
+    @DeleteMapping("/{commentId}")
+    public ApiResponse<String> deleteComment(@GetUserId Long userId, @PathVariable Long commentId) {
+        commentService.deleteComment(userId, commentId);
+        return ApiResponse.ok("정상적으로 삭제되었습니다.");
+    }
+
+}
+
