5282fa0503fa445ef23d02f8f024653ec8190542
hwangjiyoung02 <hjy020714@gmail.com>
2025-05-06 16:27:19 +0900

    🔥fix : mypage response 생성

--- src/main/java/com/blog/domain/user/controller/MyPageController.java
+++ src/main/java/com/blog/domain/user/controller/MyPageController.java
@@ -1,8 +1,11 @@
 package com.blog.domain.user.controller;
 
 import com.blog.domain.user.controller.request.UpdateRequest;
+import com.blog.domain.user.controller.response.MypageResponse;
 import com.blog.domain.user.domain.User;
 import com.blog.domain.user.service.MyPageService;
+import com.blog.global.exception.CustomException;
+import com.blog.global.exception.ErrorCode;
 import com.blog.global.response.ApiResponse;
 import com.blog.global.security.aop.GetUserId;
 import jakarta.validation.Valid;
@@ -20,18 +23,23 @@ public class MyPageController {
 
     // 정보 조회
     @GetMapping
-    public ApiResponse<User> getMyPage(@GetUserId Long userId) {
-        User myPageInfo = myPageService.getMyPageInfo(userId);
+    public ApiResponse<MypageResponse> getMyPage(@GetUserId Long userId) {
+        User user = myPageService.getMyPageInfo(userId);
+        if (user == null) {
+            throw new CustomException(ErrorCode.USER_NOT_FOUND);
+        }
+        MypageResponse myPageInfo = MypageResponse.from(user);
+
         return ApiResponse.ok(myPageInfo);
     }
 
     // 정보 수정
     @PutMapping
-    public ApiResponse<User> updateMyPage(@GetUserId Long userId,
+    public ApiResponse<String> updateMyPage(@GetUserId Long userId,
                                           @RequestBody @Valid UpdateRequest userRequest) {
 
-        User updatedUser = myPageService.updateMyPageInfo(userId, userRequest); //
-        return ApiResponse.ok(updatedUser);
+        myPageService.updateMyPageInfo(userId, userRequest);
+        return ApiResponse.ok("정상적으로 수정되었습니다.");
     }
 
 
