9d49ba8d8af664037be2721f11a10a6a18faddc5
soo0711 <k300hys@naver.com>
2025-05-13 01:13:42 +0900

    refactor: code, HttpStaus 값 중복으로 중복된 값 삭제

--- src/main/java/com/blog/common/response/ErrorCode.java
+++ src/main/java/com/blog/common/response/ErrorCode.java
@@ -4,51 +4,46 @@ import org.springframework.http.HttpStatus;
 
 public enum ErrorCode {
     // 404 Not Found
-    NOT_FOUND_POST_ID(404, HttpStatus.NOT_FOUND, "존재하지 않는 postId입니다."),
-    NOT_FOUND_END_POINT(404, HttpStatus.NOT_FOUND, "존재하지 않는 API입니다."),
-    USER_NOT_FOUND(404, HttpStatus.NOT_FOUND, "사용자 정보를 찾을 수 없습니다."),
+    NOT_FOUND_POST_ID(HttpStatus.NOT_FOUND, "존재하지 않는 postId입니다."),
+    NOT_FOUND_END_POINT(HttpStatus.NOT_FOUND, "존재하지 않는 API입니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자 정보를 찾을 수 없습니다."),
     // 403 Forbidden
-    Forbidden(403, HttpStatus.FORBIDDEN, "접속 권한이 없습니다."),
-    NO_EDIT_PERMISSION(403, HttpStatus.FORBIDDEN, "권한이 없습니다."),
+    Forbidden(HttpStatus.FORBIDDEN, "접속 권한이 없습니다."),
+    NO_EDIT_PERMISSION(HttpStatus.FORBIDDEN, "권한이 없습니다."),
 
     // 400 Bad Request
-    BAD_REQUEST(400, HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
-    DUPLICATE_NICKNAME(400, HttpStatus.BAD_REQUEST, "이미 사용 중인 닉네임입니다."),
-    DUPLICATE_EMAIL(400, HttpStatus.BAD_REQUEST, "이미 사용 중인 이메일입니다."),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    DUPLICATE_NICKNAME(HttpStatus.BAD_REQUEST, "이미 사용 중인 닉네임입니다."),
+    DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST, "이미 사용 중인 이메일입니다."),
 
     // 500 Internal Server Error
-    INTERNAL_SERVER_ERROR(500, HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다."),
-    NO_SUCH_ALGORITHM(500, HttpStatus.INTERNAL_SERVER_ERROR, "지원하지 않는 암호화 알고리즘입니다."),
-    DATABASE_ERROR(500, HttpStatus.INTERNAL_SERVER_ERROR, "데이터베이스 처리 중 오류가 발생했습니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다."),
+    NO_SUCH_ALGORITHM(HttpStatus.INTERNAL_SERVER_ERROR, "지원하지 않는 암호화 알고리즘입니다."),
+    DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "데이터베이스 처리 중 오류가 발생했습니다."),
 
     // Kakao OAuth 관련
-    KAKAO_API_ERROR(500, HttpStatus.INTERNAL_SERVER_ERROR, "카카오 API 호출 중 오류가 발생했습니다."),
-    INVALID_ACCESS_TOKEN(401, HttpStatus.UNAUTHORIZED, "유효하지 않은 카카오 액세스 토큰입니다."),
+    KAKAO_API_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "카카오 API 호출 중 오류가 발생했습니다."),
+    INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 카카오 액세스 토큰입니다."),
 
     // JWT
-    SIGNATURE_GENERATION_FAILED(500, HttpStatus.INTERNAL_SERVER_ERROR, "JWT 서명 생성 중 오류가 발생했습니다."),
-    TOKEN_PARSING_FAILED(400, HttpStatus.BAD_REQUEST, "JWT 토큰 파싱 중 오류가 발생했습니다."),
-    INVALID_TOKEN(401, HttpStatus.UNAUTHORIZED, "로그인이 만료되었거나 유효하지 않은 토큰입니다. 다시 로그인해주세요."),
-    EXPIRED_TOKEN(401, HttpStatus.UNAUTHORIZED, "JWT 토큰이 만료되었습니다."),
-    TOKEN_USERID_EXTRACTION_FAILED(500, HttpStatus.INTERNAL_SERVER_ERROR, "JWT 토큰에서 사용자 ID 추출 중 오류가 발생했습니다."),
-    TOKEN_EMAIL_EXTRACTION_FAILED(500, HttpStatus.INTERNAL_SERVER_ERROR, "JWT 토큰에서 이메일 추출 중 오류가 발생했습니다."),
-    JSON_CONVERT_FAILED(500, HttpStatus.INTERNAL_SERVER_ERROR, "JWT 생성 중 JSON 변환 오류가 발생했습니다.");
+    SIGNATURE_GENERATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "JWT 서명 생성 중 오류가 발생했습니다."),
+    TOKEN_PARSING_FAILED(HttpStatus.BAD_REQUEST, "JWT 토큰 파싱 중 오류가 발생했습니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "로그인이 만료되었거나 유효하지 않은 토큰입니다. 다시 로그인해주세요."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "JWT 토큰이 만료되었습니다."),
+    TOKEN_USERID_EXTRACTION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "JWT 토큰에서 사용자 ID 추출 중 오류가 발생했습니다."),
+    TOKEN_EMAIL_EXTRACTION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "JWT 토큰에서 이메일 추출 중 오류가 발생했습니다."),
+    JSON_CONVERT_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "JWT 생성 중 JSON 변환 오류가 발생했습니다.");
 
 
 
-    private final Integer code;
     private final HttpStatus httpStatus;
     private final String message;
 
-    ErrorCode(Integer code, HttpStatus httpStatus, String message){
-        this.code = code;
+    ErrorCode(HttpStatus httpStatus, String message){
         this.httpStatus = httpStatus;
         this.message = message;
     }
 
-    public Integer getCode() {
-        return code;
-    }
 
     public HttpStatus getHttpStatus() {
         return httpStatus;
--- src/main/java/com/blog/common/response/ExceptionDto.java
+++ src/main/java/com/blog/common/response/ExceptionDto.java
@@ -3,12 +3,10 @@ package com.blog.common.response;
 import jakarta.validation.constraints.NotNull;
 
 public record ExceptionDto(
-        @NotNull Integer code,
         @NotNull String message
 ) {
     public static ExceptionDto of(ErrorCode errorCode) {
         return new ExceptionDto(
-                errorCode.getCode(),
                 errorCode.getMessage()
         );
     }
