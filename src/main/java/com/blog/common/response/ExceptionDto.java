2bfce4c65cd7c11c813026af33308a714bc41ad8
soo0711 <k300hys@naver.com>
2025-05-19 15:39:43 +0900

    refactor : erroCode return 값 HttpStatus 추가

--- src/main/java/com/blog/common/response/ExceptionDto.java
+++ src/main/java/com/blog/common/response/ExceptionDto.java
@@ -1,12 +1,15 @@
 package com.blog.common.response;
 
 import jakarta.validation.constraints.NotNull;
+import org.springframework.http.HttpStatus;
 
 public record ExceptionDto(
+        @NotNull HttpStatus httpStatus,
         @NotNull String message
 ) {
     public static ExceptionDto of(ErrorCode errorCode) {
         return new ExceptionDto(
+                errorCode.getHttpStatus(),
                 errorCode.getMessage()
         );
     }
